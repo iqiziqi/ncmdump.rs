2c54aee84120b1acf16b767083cfc96a6f323fd8
haiyoune7 <haiyoune7@outlook.com>
2019-07-15 15:50:06 +0800

    [update] implement basic ncmdump

---
+++ src/lib.rs
@@ -0,0 +1,135 @@
+extern crate aes_soft;
+extern crate base64;
+extern crate block_modes;
+extern crate rustc_serialize;
+
+use std::error;
+use std::fs::File;
+use std::io;
+use std::io::{BufReader, BufWriter, Read, Write};
+
+use aes_soft::Aes128;
+use block_modes::block_padding::Pkcs7;
+use block_modes::{BlockMode, Ecb};
+use rustc_serialize::json::Json;
+
+type Aes128Ecb = Ecb<Aes128, Pkcs7>;
+
+static HEADER_KEY: [u8; 16] = [
+    0x68, 0x7A, 0x48, 0x52, 0x41, 0x6D, 0x73, 0x6F,
+    0x35, 0x6B, 0x49, 0x6E, 0x62, 0x61, 0x78, 0x57,
+];
+
+static MODIFY_KEY: [u8; 16] = [
+    0x23, 0x31, 0x34, 0x6C, 0x6A, 0x6B, 0x5F, 0x21,
+    0x5C, 0x5D, 0x26, 0x30, 0x55, 0x3C, 0x27, 0x28,
+];
+
+pub fn process(input: &String) -> Result<(), Box<dyn error::Error>> {
+    let file = File::open(input)?;
+    let mut reader = BufReader::new(&file);
+
+    let _ = check_format(&mut reader)?;
+    let key = get_key(&mut reader)?;
+    let modify = get_modify(&mut reader)?;
+    let _ = crc_check(&mut reader)?;
+    let _ = get_image(&mut reader)?;
+
+    let key_box = build_key_box(&key);
+    let output_file = File::create("./demo.flac")?;
+    let mut writer = BufWriter::new(&output_file);
+    write_file(&mut reader, &mut writer, &key_box)?;
+
+    Ok(())
+}
+
+fn decrypt(data: &[u8], key: &[u8]) -> Vec<u8> {
+    let cipher = Aes128Ecb::new_var(&key, &[]).unwrap();
+    cipher.decrypt_vec(data).unwrap()
+}
+
+fn check_format(reader: &mut BufReader<&File>) -> io::Result<()> {
+    let mut buffer = [0u8; 8];
+    reader.read(&mut buffer)?;
+    if u64::from_ne_bytes(buffer) != 0x4d4144464e455443 {
+        return Err(io::Error::from(io::ErrorKind::InvalidData));
+    }
+    reader.read(&mut [0u8; 2])?;
+    Ok(())
+}
+
+fn get_length(reader: &mut BufReader<&File>) -> io::Result<u32> {
+    let mut buffer = [0u8; 4];
+    reader.read(&mut buffer)?;
+
+    Ok(u32::from_ne_bytes(buffer))
+}
+
+fn get_key(reader: &mut BufReader<&File>) -> io::Result<Vec<u8>> {
+    let length = get_length(reader)?;
+    let mut buffer = vec![0u8; length as usize];
+    reader.read_exact(&mut buffer)?;
+
+    let key_buffer = buffer.iter().map(|byte| byte ^ 0x64).collect::<Vec<u8>>();
+    let decrypt_buffer = decrypt(&key_buffer, &HEADER_KEY);
+    Ok(decrypt_buffer[17..].to_vec())
+}
+
+fn get_modify(reader: &mut BufReader<&File>) -> Result<(), Box<dyn error::Error>> {
+    let length = get_length(reader)?;
+    let mut buffer = vec![0u8; length as usize];
+    reader.read_exact(&mut buffer)?;
+
+    let modify_tmp = buffer.iter().map(|item| item ^ 0x63).collect::<Vec<u8>>();
+    let modify_key = base64::decode(&modify_tmp[22..])?;
+    let modify_str = String::from_utf8(decrypt(&modify_key, &MODIFY_KEY)[6..].to_vec())?;
+    let modify = Json::from_str(&modify_str)?;
+
+    Ok(())
+}
+
+fn crc_check(reader: &mut BufReader<&File>) -> Result<(), io::Error> {
+    let mut buffer = [0u8; 9];
+    reader.read(&mut buffer)?;
+    Ok(())
+}
+
+fn get_image(reader: &mut BufReader<&File>) -> Result<Vec<u8>, io::Error> {
+    let length = get_length(reader)?;
+    let mut buffer = vec![0u8; length as usize];
+    reader.read_exact(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn write_file(
+    reader: &mut BufReader<&File>,
+    writer: &mut BufWriter<&File>,
+    key_box: &Vec<usize>,
+) -> Result<(), io::Error> {
+    let mut write_buffer = [0u8; 0x8000];
+    while reader.read(&mut write_buffer)? > 0 {
+        for i in 0..0x8000 {
+            let j = (i + 1) & 0xff;
+            write_buffer[i] ^= key_box[key_box[j] + key_box[(key_box[j] + j) & 0xff] & 0xff] as u8;
+        }
+        writer.write(&mut write_buffer)?;
+    }
+    writer.flush()?;
+
+    Ok(())
+}
+
+fn build_key_box(key: &Vec<u8>) -> Vec<usize> {
+    let mut key_box = (0..256).collect::<Vec<usize>>();
+    let mut last_byte = 0;
+    let mut offsets = (0..key.len()).cycle();
+
+    for i in 0..256 {
+        let offset = offsets.next().unwrap();
+        let c = (key_box[i] + last_byte + key[offset] as usize) & 0xff;
+        key_box.swap(i, c);
+        last_byte = c;
+    }
+
+    key_box
+}
