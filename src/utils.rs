f1d005af729c3a8328aee0b0f394289e0cbc346e
haiyoune7 <haiyoune7@outlook.com>
2019-09-18 15:05:00 +0800

    [update] add simple unit test

--- src/utils.rs
+++ src/utils.rs
@@ -33,3 +33,39 @@ pub fn get_length(buffer: &[u8]) -> Result<u32, Error> {
     let length = u32::from_ne_bytes(bytes);
     Ok(length)
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_get_n_element() {
+        let collection = (0..99).collect::<Vec<u8>>();
+        let mut iter: Iter<u8> = collection.iter();
+
+        assert_eq!(get_n_element(&mut iter, 4).unwrap(), vec![0, 1, 2, 3]);
+        assert_eq!(get_n_element(&mut iter, 4).unwrap(), vec![4, 5, 6, 7]);
+        assert_eq!(get_n_element(&mut iter, 2).unwrap(), vec![8, 9]);
+    }
+
+    #[test]
+    fn test_check_format() {
+        let buffer = [67, 84, 69, 78, 70, 68, 65, 77];
+        check_format(&buffer).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_check_format_panic() {
+        let buffer = [67, 84, 69, 78, 70, 68, 65, 76];
+        check_format(&buffer).unwrap();
+    }
+
+    #[test]
+    fn test_get_length() {
+        assert_eq!(get_length(&[0x00, 0x00, 0x00, 0x00]).unwrap(), 0);
+        assert_eq!(get_length(&[0x4d, 0x41, 0x44, 0x46]).unwrap(), 1_178_878_285);
+        assert_eq!(get_length(&[0x4e, 0x45, 0x54, 0x43]).unwrap(), 1_129_596_238);
+    }
+}
