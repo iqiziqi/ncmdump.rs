c8f40dbd05284ce8d498f30ae45c0e6a98c353d1
haiyoune7 <haiyoune7@outlook.com>
2019-09-21 14:08:46 +0800

    [docs] add comment to error mod

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,11 @@
 use std::error;
 use std::fmt::{Display, Formatter, Result};
 
+/// The error type for ncmdump.
+///
+/// It contains kind and msg fields,
+/// you can get it by `kind` method and `msg` method,
+/// them are useful.
 #[derive(Debug)]
 pub struct Error {
     kind: ErrorKind,
@@ -21,14 +26,54 @@ impl From<ErrorKind> for Error {
 impl error::Error for Error {}
 
 impl Error {
+
+    /// Create a new Error from a kind of error.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// extern crate ncmdump;
+    ///
+    /// use ncmdump::error::{Error, ErrorKind};
+    ///
+    /// let err = Error::new(ErrorKind::Unknown);
+    /// ```
     pub fn new(kind: ErrorKind) -> Error {
         Error { kind }
     }
 
+    /// Get the error kind
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # extern crate ncmdump;
+    /// # use ncmdump::error::{Error, ErrorKind};
+    /// #
+    /// # let err = Error::new(ErrorKind::Unknown);
+    /// #
+    /// println!("{:?}", err.kind());
+    ///
+    /// // Output: Unknown
+    /// ```
     pub fn kind(&self) -> ErrorKind {
         self.kind
     }
 
+    /// Get the error message
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # extern crate ncmdump;
+    /// # use ncmdump::error::{Error, ErrorKind};
+    /// #
+    /// # let err = Error::new(ErrorKind::Unknown);
+    /// #
+    /// println!("{}", err.msg());
+    ///
+    /// // Output: Unknown error
+    /// ```
     pub fn msg(&self) -> &str {
         match self.kind() {
             ErrorKind::InvalidFile => "Invalid file",
@@ -38,9 +83,13 @@ impl Error {
     }
 }
 
+/// A list specifying general categories of ncmdump error.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum ErrorKind {
+    /// The format of file is invalid
     InvalidFile,
+    /// Can't decode modify of this file
     ModifyDecodeError,
+    /// Unknown error
     Unknown,
 }
