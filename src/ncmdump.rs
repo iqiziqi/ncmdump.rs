910842faa80e025e24d8e24adb97ac6e63ef83b4
iqiziqi <iqiziqi@outlook.com>
2023-01-17 18:24:12 +0800

    [refac] Improve the structure of lib

--- src/ncmdump.rs
+++ src/ncmdump.rs
@@ -3,10 +3,36 @@ use std::io::{Read, Seek, SeekFrom};
 use anyhow::Result;
 use base64::engine::general_purpose::STANDARD;
 use base64::Engine;
+use serde::{Deserialize, Serialize};
 
-use crate::decrypt::{build_key_box, decrypt, HEADER_KEY, MODIFY_KEY};
+use crate::decrypt::{build_key_box, decrypt, HEADER_KEY, INFO_KEY};
 use crate::error::Errors;
-use crate::Modify;
+
+/// The music information
+#[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct NcmInfo {
+    /// The name of music
+    #[serde(rename = "musicName")]
+    pub name: String,
+    /// The id of music
+    #[serde(rename = "musicId")]
+    pub id: u64,
+    /// The album of music, it's an url
+    pub album: String,
+    /// The artist of music, first item is name, second item is id
+    pub artist: Vec<(String, u64)>,
+    // The bit rate of music
+    pub bitrate: u64,
+    /// The duration of music
+    pub duration: u64,
+    /// The format of music, is maybe 'mp3' or 'flac'
+    pub format: String,
+    /// The id of MV
+    #[serde(rename = "mvId")]
+    pub mv_id: Option<u64>,
+    /// The alias of music
+    pub alias: Option<Vec<String>>,
+}
 
 pub struct Ncmdump<S>
 where
@@ -14,7 +40,7 @@ where
 {
     reader: S,
     key: (u64, u64),
-    modify: (u64, u64),
+    info: (u64, u64),
     image: (u64, u64),
 }
 
@@ -84,15 +110,15 @@ where
         let key_length = Self::get_length(&key_length_buffer)?;
 
         reader.seek(SeekFrom::Current(key_length as i64))?;
-        let mut modify_length_buffer = [0; 4];
-        let read_size = reader.read(&mut modify_length_buffer)? as u64;
+        let mut info_length_buffer = [0; 4];
+        let read_size = reader.read(&mut info_length_buffer)? as u64;
         if read_size != 4 {
-            return Err(Errors::InvalidModifyLength.into());
+            return Err(Errors::InvalidInfoLength.into());
         }
-        let modify_start = reader.stream_position()?;
-        let modify_length = Self::get_length(&modify_length_buffer)?;
+        let info_start = reader.stream_position()?;
+        let info_length = Self::get_length(&info_length_buffer)?;
 
-        reader.seek(SeekFrom::Current(modify_length as i64))?;
+        reader.seek(SeekFrom::Current(info_length as i64))?;
         reader.seek(SeekFrom::Current(9))?;
         let mut image_length_buffer = [0; 4];
         let read_size = reader.read(&mut image_length_buffer)?;
@@ -105,7 +131,7 @@ where
         Ok(Self {
             reader,
             key: (key_start, key_length),
-            modify: (modify_start, modify_length),
+            info: (info_start, info_length),
             image: (image_start, image_length),
         })
     }
@@ -128,7 +154,7 @@ where
         Ok(decrypt_buffer[17..].to_vec())
     }
 
-    /// Decode the modify buffer and just return the file modify.
+    /// Decode the information buffer and just return the information.
     ///
     /// # Example
     ///
@@ -142,24 +168,27 @@ where
     /// fn main() -> Result<()> {
     ///     let file = File::open("tests/test.ncm")?;
     ///     let mut ncm = Ncmdump::from_reader(file)?;
-    ///     let modify = ncm.get_info();
-    ///     println!("{:?}", modify);
+    ///     let info = ncm.get_info();
+    ///     println!("{:?}", info);
     ///     Ok(())
     /// }
     /// ```
-    pub fn get_info(&mut self) -> Result<Modify> {
-        let (start, length) = self.modify;
-        let modify = self.get_bytes(start, length)?;
-        let modify_tmp = modify.iter().map(|item| item ^ 0x63).collect::<Vec<u8>>();
-        let modify_key = STANDARD
-            .decode(&modify_tmp[22..])
-            .map_err(|_| Errors::ModifyDecodeError)?;
-        let modify_data = decrypt(&modify_key, &MODIFY_KEY)?;
-        let modify_str =
-            String::from_utf8(modify_data[6..].to_vec()).map_err(|_| Errors::ModifyDecodeError)?;
-        let modify =
-            serde_json::from_str::<Modify>(&modify_str).map_err(|_| Errors::ModifyDecodeError)?;
-        Ok(modify)
+    pub fn get_info(&mut self) -> Result<NcmInfo> {
+        let (start, length) = self.info;
+        let info_bytes = self.get_bytes(start, length)?;
+        let info_tmp = info_bytes
+            .iter()
+            .map(|item| item ^ 0x63)
+            .collect::<Vec<u8>>();
+        let info_key = STANDARD
+            .decode(&info_tmp[22..])
+            .map_err(|_| Errors::InfoDecodeError)?;
+        let info_data = decrypt(&info_key, &INFO_KEY)?;
+        let info_str =
+            String::from_utf8(info_data[6..].to_vec()).map_err(|_| Errors::InfoDecodeError)?;
+        let info =
+            serde_json::from_str::<NcmInfo>(&info_str).map_err(|_| Errors::InfoDecodeError)?;
+        Ok(info)
     }
 
     /// Get the image bytes from ncmdump, if it's exists.
@@ -245,7 +274,16 @@ pub mod tests {
     use anyhow::Result;
 
     use super::*;
-    use crate::{get_blocks, get_key, get_modify};
+
+    const KEY: [u8; 96] = [
+        0x31, 0x31, 0x38, 0x31, 0x39, 0x38, 0x30, 0x33, 0x33, 0x32, 0x38, 0x35, 0x45, 0x37, 0x66,
+        0x54, 0x34, 0x39, 0x78, 0x37, 0x64, 0x6f, 0x66, 0x39, 0x4f, 0x4b, 0x43, 0x67, 0x67, 0x39,
+        0x63, 0x64, 0x76, 0x68, 0x45, 0x75, 0x65, 0x7a, 0x79, 0x33, 0x69, 0x5a, 0x43, 0x4c, 0x31,
+        0x6e, 0x46, 0x76, 0x42, 0x46, 0x64, 0x31, 0x54, 0x34, 0x75, 0x53, 0x6b, 0x74, 0x41, 0x4a,
+        0x4b, 0x6d, 0x77, 0x5a, 0x58, 0x73, 0x69, 0x6a, 0x50, 0x62, 0x69, 0x6a, 0x6c, 0x69, 0x69,
+        0x6f, 0x6e, 0x56, 0x55, 0x58, 0x58, 0x67, 0x39, 0x70, 0x6c, 0x54, 0x62, 0x58, 0x45, 0x63,
+        0x6c, 0x41, 0x45, 0x39, 0x4c, 0x62,
+    ];
 
     #[test]
     fn test_create_dump_ok() -> Result<()> {
@@ -258,31 +296,33 @@ pub mod tests {
     fn test_get_key_ok() -> Result<()> {
         let reader = File::open("./tests/test.ncm")?;
         let mut ncm = Ncmdump::from_reader(reader)?;
-        let key_new = ncm.get_key()?;
+        let key = ncm.get_key()?;
 
-        let mut reader = File::open("./tests/test.ncm")?;
-        let mut data = Vec::new();
-        reader.read_to_end(&mut data)?;
-        let blocks = get_blocks(&data)?;
-        let key_old = get_key(blocks.key)?;
+        assert_eq!(key[..], KEY[..]);
 
-        assert_eq!(key_new, key_old);
         Ok(())
     }
 
     #[test]
-    fn test_get_modify_ok() -> Result<()> {
-        let mut reader = File::open("./tests/test.ncm")?;
-        let mut data = Vec::new();
-        reader.read_to_end(&mut data)?;
-        let blocks = get_blocks(&data)?;
-        let modify_old = get_modify(blocks.modify)?;
-
+    fn test_get_info_ok() -> Result<()> {
         let reader = File::open("./tests/test.ncm")?;
         let mut ncm = Ncmdump::from_reader(reader)?;
-        let modify_new = ncm.get_info()?;
+        let info = ncm.get_info()?;
 
-        assert_eq!(modify_new, modify_old);
+        assert_eq!(
+            info,
+            NcmInfo {
+                name: "寒鸦少年".to_string(),
+                id: 1305366556,
+                album: "寒鸦少年".to_string(),
+                artist: vec![("华晨宇".into(), 861777)],
+                bitrate: 923378,
+                duration: 315146,
+                format: "flac".to_string(),
+                mv_id: Some(0),
+                alias: Some(vec!["电视剧《斗破苍穹》主题曲".into()]),
+            },
+        );
         Ok(())
     }
 
@@ -290,15 +330,53 @@ pub mod tests {
     fn test_get_image_ok() -> Result<()> {
         let reader = File::open("./tests/test.ncm")?;
         let mut ncm = Ncmdump::from_reader(reader)?;
-        let image_new = ncm.get_image()?;
+        let image = ncm.get_image()?;
+        let length = image.len();
 
-        let mut reader = File::open("./tests/test.ncm")?;
-        let mut data = Vec::new();
-        reader.read_to_end(&mut data)?;
-        let blocks = get_blocks(&data)?;
-        let image_old = blocks.image.to_vec();
+        println!("length = {}, image: {:x?}", length, image);
 
-        assert_eq!(image_new, image_old);
+        assert_eq!(length, 39009);
+        assert_eq!(
+            image[..16],
+            [
+                0xff, 0xd8, 0xff, 0xe0, 0x00, 0x10, 0x4a, 0x46, 0x49, 0x46, 0x00, 0x01, 0x01, 0x01,
+                0x00, 0x48,
+            ],
+        );
+        assert_eq!(
+            image[38993..],
+            [
+                0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                0xff, 0xd9,
+            ],
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_data_ok() -> Result<()> {
+        let reader = File::open("./tests/test.ncm")?;
+        let mut ncm = Ncmdump::from_reader(reader)?;
+        let data = ncm.get_data()?;
+        let length = data.len();
+        println!("length = {}, image: {:x?}", length, data);
+
+        assert_eq!(length, 61440);
+        assert_eq!(
+            data[..16],
+            [
+                0x66, 0x4c, 0x61, 0x43, 0x00, 0x00, 0x00, 0x22, 0x12, 0x00, 0x12, 0x00, 0x00, 0x01,
+                0x01, 0x00,
+            ],
+        );
+        assert_eq!(
+            data[61424..],
+            [
+                0x8b, 0x25, 0x88, 0x08, 0x4b, 0x49, 0x89, 0xc2, 0xba, 0xe3, 0xda, 0x88, 0x48, 0xc1,
+                0x09, 0x7b,
+            ],
+        );
         Ok(())
     }
 }
