09e01eeab637d4053a88929fcf50b6ee9bb6efcc
Puqns67 <me@puqns67.icu>
2023-03-01 19:35:03 +0800

    [chore] Optimize progress bar display

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,25 @@ use std::path::{Path, PathBuf};
 use anyhow::Result;
 use clap::Parser;
 use glob::glob;
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use thiserror::Error;
 
-use ncmdump::{Ncmdump, QmcDump};
+#[cfg(feature = "ncmdump")]
+use ncmdump::Ncmdump;
+#[cfg(feature = "qmcdump")]
+use ncmdump::QmcDump;
+
+const PROGRESS_STYLE_RUN: &str = "[{elapsed_precise:.blue}] [{bar:40.cyan}] {pos:>10!.cyan}/{len:<10!.blue} | {percent:>3!}% | {msg}";
+const PROGRESS_STYLE_DUMP: &str = "[{elapsed_precise:.blue}] [{bar:40.cyan}] {bytes:>10!.cyan}/{total_bytes:<10!.blue} | {percent:>3!}% | {bytes_per_sec}";
+const PROGRESS_STYLE_BAR: &str = "=> ";
+
+enum FileType {
+    #[cfg(feature = "ncmdump")]
+    Ncm,
+    #[cfg(feature = "qmcdump")]
+    Qmc,
+    Other,
+}
 
 #[derive(Clone, Debug, Error)]
 enum Error {
@@ -16,17 +31,8 @@ enum Error {
     PathError,
     #[error("Invalid file format")]
     FormatError,
-}
-
-struct Wrapper {
-    path: PathBuf,
-    size: u64,
-    name: String,
-}
-
-enum FileType {
-    Ncm,
-    Qmc,
+    #[error("No file can be converted")]
+    NoFileError,
 }
 
 #[derive(Debug, Parser)]
@@ -46,95 +52,199 @@ struct Command {
     verbose: bool,
 }
 
-fn check_format(path: &PathBuf) -> Result<FileType> {
-    let mut file = File::open(path)?;
-    let mut head = [0; 8];
-    let count = file.read(&mut head)?;
-    if count != 8 {
-        return Err(Error::FormatError.into());
+struct Wrapper {
+    name: String,
+    format: FileType,
+    path: PathBuf,
+    size: u64,
+}
+
+impl Wrapper {
+    fn from_path(path: PathBuf) -> Result<Self> {
+        let mut file = File::open(&path)?;
+        let mut head = [0; 8];
+        let format = if file.read(&mut head)? == 8 {
+            match head[..] {
+                #[cfg(feature = "ncmdump")]
+                [0x43, 0x54, 0x45, 0x4E, 0x46, 0x44, 0x41, 0x4D] => FileType::Ncm,
+                #[cfg(feature = "qmcdump")]
+                [0xA5, 0x06, 0xB7, 0x89, _, _, _, _] => FileType::Qmc,
+                #[cfg(feature = "qmcdump")]
+                [0x8A, 0x0E, 0xE5, _, _, _, _, _] => FileType::Qmc,
+                _ => FileType::Other,
+            }
+        } else {
+            FileType::Other
+        };
+
+        Ok(Self {
+            name: path.file_name().unwrap().to_str().unwrap().to_string(),
+            format,
+            path,
+            size: file.metadata().unwrap().len(),
+        })
     }
-    let file_type = match head[..] {
-        [0x43, 0x54, 0x45, 0x4E, 0x46, 0x44, 0x41, 0x4D] => FileType::Ncm,
-        [0xA5, 0x06, 0xB7, 0x89, _, _, _, _] => FileType::Qmc,
-        [0x8A, 0x0E, 0xE5, _, _, _, _, _] => FileType::Qmc,
-        _ => return Err(Error::FormatError.into()),
-    };
-    Ok(file_type)
 }
 
-fn get_output(file_path: &Path, format: &str, output: &Option<String>) -> Result<PathBuf> {
-    let parent = match output {
-        None => file_path.parent().ok_or(Error::PathError)?,
-        Some(p) => Path::new(p),
-    };
-    let file_name = file_path.file_stem().ok_or(Error::PathError)?;
-    let path = parent.join(file_name).with_extension(format);
-    Ok(path)
+struct NcmdumpCli {
+    command: Command,
+    progress: MultiProgress,
 }
 
-fn get_paths(matchers: &Vec<String>) -> Result<Vec<Wrapper>> {
-    let mut paths = Vec::new();
-    for matcher in matchers {
-        for entry in glob(matcher)? {
-            let path = entry?;
-            let file = File::open(&path)?;
-            let size = file.metadata()?.len();
-            let name = path.file_name().unwrap().to_str().unwrap().to_string();
-            paths.push(Wrapper { path, size, name })
+impl NcmdumpCli {
+    fn from_command(command: Command) -> Self {
+        Self {
+            command,
+            progress: MultiProgress::new(),
         }
     }
-    Ok(paths)
-}
 
-fn main() -> Result<()> {
-    let command = Command::parse();
+    fn get_output(
+        &self,
+        file_path: &Path,
+        format: &str,
+        output: &Option<String>,
+    ) -> Result<PathBuf> {
+        let parent = match output {
+            None => file_path.parent().ok_or(Error::PathError)?,
+            Some(p) => Path::new(p),
+        };
+        let file_name = file_path.file_stem().ok_or(Error::PathError)?;
+        let path = parent.join(file_name).with_extension(format);
+        Ok(path)
+    }
 
-    let list = get_paths(&command.matchers)?;
-    let total_size = list.iter().map(|item| item.size).sum();
-    let template = "{bytes:>10!}/{total_bytes:<10!} {bytes_per_sec:>15!} [{bar:40}]";
-    let progress_style = ProgressStyle::with_template(template)?;
-    let progress = ProgressBar::new(total_size).with_style(progress_style);
-    for item in list {
-        let file_type = check_format(&item.path)?;
+    fn get_paths(&self) -> Result<Vec<PathBuf>> {
+        let mut paths = Vec::new();
+        for matcher in &self.command.matchers {
+            for entry in glob(matcher.as_str())? {
+                match entry {
+                    Ok(path) => {
+                        if path.is_file() {
+                            paths.push(path);
+                        }
+                    }
+                    Err(e) => println!("{:?}", e),
+                }
+            }
+        }
+        Ok(paths)
+    }
+
+    fn get_info(&self, paths: Vec<PathBuf>, progress: &ProgressBar) -> Vec<Wrapper> {
+        let mut result = Vec::new();
+        for path in paths {
+            progress.set_message(path.file_name().unwrap().to_str().unwrap().to_string());
+            if let Ok(item) = Wrapper::from_path(path) {
+                match item.format {
+                    FileType::Other => {}
+                    _ => result.push(item),
+                }
+            };
+            progress.inc(1)
+        }
+        progress.finish();
+        result
+    }
+
+    fn get_data(&self, mut dump: impl Read, progress: &ProgressBar) -> Result<Vec<u8>> {
         let mut data = Vec::new();
-        match file_type {
-            FileType::Ncm => {
-                let file = File::open(&item.path)?;
-                let mut dump = Ncmdump::from_reader(file)?;
-                let mut buffer = [0; 1024];
-                while let Ok(size) = dump.read(&mut buffer) {
-                    if size == 0 {
-                        break;
-                    }
-                    data.write_all(&buffer[..size])?;
-                    progress.inc(size as u64);
-                }
+        let mut buffer = [0; 1024];
+        while let Ok(size) = dump.read(&mut buffer) {
+            if size == 0 {
+                break;
             }
-            FileType::Qmc => {
-                let file = File::open(&item.path)?;
-                let mut dump = QmcDump::from_reader(file)?;
-                let mut buffer = [0; 1024];
-                while let Ok(size) = dump.read(&mut buffer) {
-                    if size == 0 {
-                        break;
-                    }
-                    data.write_all(&buffer[..size])?;
-                    progress.inc(size as u64);
-                }
-            }
+            data.write_all(&buffer[..size])?;
+            progress.inc(size as u64);
         }
+        progress.finish();
+        Ok(data)
+    }
+
+    fn dump(&self, item: &Wrapper, progress: &ProgressBar) -> Result<()> {
+        let file = File::open(&item.path)?;
+        let data = match item.format {
+            #[cfg(feature = "ncmdump")]
+            FileType::Ncm => self.get_data(Ncmdump::from_reader(file)?, progress),
+            #[cfg(feature = "qmcdump")]
+            FileType::Qmc => self.get_data(QmcDump::from_reader(file)?, progress),
+            FileType::Other => Err(Error::FormatError.into()),
+        }?;
         let ext = match data[..4] {
             [0x66, 0x4C, 0x61, 0x43] => Ok("flac"),
             [0x49, 0x44, 0x33, _] => Ok("mp3"),
             _ => Err(Error::FormatError),
         }?;
-        let output_file = get_output(&item.path, ext, &command.output)?;
+        let output_file = self.get_output(&item.path, ext, &self.command.output)?;
         let mut target = File::options().create(true).write(true).open(output_file)?;
         target.write_all(&data)?;
-        if command.verbose {
-            progress.println(format!("Converting file {}\t complete!", item.name));
-        }
+        Ok(())
     }
-    progress.finish();
-    Ok(())
+
+    fn start(&self) -> Result<()> {
+        if self.command.matchers.is_empty() {
+            return Err(Error::NoFileError.into());
+        }
+
+        let progress_style_run =
+            ProgressStyle::with_template(PROGRESS_STYLE_RUN)?.progress_chars(PROGRESS_STYLE_BAR);
+        let progress_style_dump =
+            ProgressStyle::with_template(PROGRESS_STYLE_DUMP)?.progress_chars(PROGRESS_STYLE_BAR);
+
+        let paths = self.get_paths()?;
+        let progress_info = self
+            .progress
+            .add(ProgressBar::new(paths.len() as u64))
+            .with_style(progress_style_run.clone());
+        let items = self.get_info(paths, &progress_info);
+
+        match items.len() {
+            0 => return Err(Error::NoFileError.into()),
+            1 => {
+                let item = items.get(0).unwrap();
+                let progress = self
+                    .progress
+                    .add(ProgressBar::new(item.size).with_style(progress_style_dump));
+                self.dump(item, &progress)?;
+                if self.command.verbose {
+                    self.progress
+                        .println(format!("Converting file {}\t complete!", item.name))?;
+                }
+            }
+            _ => {
+                let progress_run = self
+                    .progress
+                    .add(ProgressBar::new(items.len() as u64).with_style(progress_style_run));
+                let progress_dump = self
+                    .progress
+                    .add(ProgressBar::new(1).with_style(progress_style_dump));
+
+                for item in items {
+                    progress_run.set_message(item.name.clone());
+                    progress_dump.reset();
+                    progress_dump.set_length(item.size);
+                    match self.dump(&item, &progress_dump) {
+                        Ok(_) => {
+                            if self.command.verbose {
+                                self.progress.println(format!(
+                                    "Converting file {}\t complete!",
+                                    item.name
+                                ))?;
+                            }
+                            progress_run.inc(1);
+                        }
+                        Err(e) => println!("{:?}", e),
+                    }
+                }
+                progress_run.finish();
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let app = NcmdumpCli::from_command(Command::parse());
+    app.start()
 }
