005085bd03b22ee482f56e6bde8028ee372e8b3f
iqiziqi <iqiziqi@outlook.com>
2023-09-15 23:14:07 +0800

    [feat] Add filetype parse

--- crates/ncmdump/src/utils.rs
+++ crates/ncmdump/src/utils.rs
@@ -14,6 +14,42 @@ pub enum FileType {
     Other,
 }
 
+impl FileType {
+    /// Return the file type of the reader.
+    ///
+    /// > Notice: This function can't resolve the `Ncmdump` or `QmcDump`
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::fs::File;
+    /// # use ncmdump::utils::FileType;
+    /// #
+    /// let mut file = File::open("./tests/test.ncm").unwrap();
+    /// let file_type = FileType::parse(&mut file).unwrap();
+    /// ```
+    pub fn parse<R>(reader: &mut R) -> Result<Self>
+    where
+        R: Read,
+    {
+        let mut head = [0; 8];
+        if reader.read(&mut head)? != 8 {
+            return Ok(FileType::Other);
+        }
+
+        let file_type = match head[..] {
+            #[cfg(feature = "ncmdump")]
+            [0x43, 0x54, 0x45, 0x4E, 0x46, 0x44, 0x41, 0x4D] => FileType::Ncm,
+            #[cfg(feature = "qmcdump")]
+            [0xA5, 0x06, 0xB7, 0x89, _, _, _, _] => FileType::Qmc,
+            #[cfg(feature = "qmcdump")]
+            [0x8A, 0x0E, 0xE5, _, _, _, _, _] => FileType::Qmc,
+            _ => FileType::Other,
+        };
+        Ok(file_type)
+    }
+}
+
 /// Return the file type of the reader.
 ///
 /// > Notice: This function can't resolve the `Ncmdump` or `QmcDump`
@@ -27,25 +63,12 @@ pub enum FileType {
 /// let mut file = File::open("./tests/test.ncm").unwrap();
 /// let file_type = get_file_type(&mut file).unwrap();
 /// ```
+#[deprecated]
 pub fn get_file_type<R>(reader: &mut R) -> Result<FileType>
 where
     R: Read,
 {
-    let mut head = [0; 8];
-    if reader.read(&mut head)? != 8 {
-        return Ok(FileType::Other);
-    }
-
-    let file_type = match head[..] {
-        #[cfg(feature = "ncmdump")]
-        [0x43, 0x54, 0x45, 0x4E, 0x46, 0x44, 0x41, 0x4D] => FileType::Ncm,
-        #[cfg(feature = "qmcdump")]
-        [0xA5, 0x06, 0xB7, 0x89, _, _, _, _] => FileType::Qmc,
-        #[cfg(feature = "qmcdump")]
-        [0x8A, 0x0E, 0xE5, _, _, _, _, _] => FileType::Qmc,
-        _ => FileType::Other,
-    };
-    Ok(file_type)
+    FileType::parse(reader)
 }
 
 /// Check if the reader is ncm format.
@@ -66,7 +89,7 @@ pub fn is_ncm_file<R>(reader: &mut R) -> Result<bool>
 where
     R: Read,
 {
-    let file_type = get_file_type(reader)?;
+    let file_type = FileType::parse(reader)?;
     Ok(file_type == FileType::Ncm)
 }
 
@@ -88,7 +111,7 @@ pub fn is_qmc_file<R>(reader: &mut R) -> Result<bool>
 where
     R: Read,
 {
-    let file_type = get_file_type(reader)?;
+    let file_type = FileType::parse(reader)?;
     Ok(file_type == FileType::Qmc)
 }
 
@@ -97,7 +120,7 @@ mod tests {
     use std::fs::File;
     use std::io::Error;
 
-    use crate::utils::{get_file_type, is_ncm_file, FileType};
+    use crate::utils::{is_ncm_file, FileType};
 
     #[cfg(feature = "ncmdump")]
     #[test]
@@ -109,10 +132,11 @@ mod tests {
         Ok(())
     }
 
+    #[cfg(feature = "ncmdump")]
     #[test]
     fn test_get_file_type_ok() -> Result<(), Error> {
         let mut file = File::open("./tests/test.ncm")?;
-        let file_type = get_file_type(&mut file);
+        let file_type = FileType::parse(&mut file);
         assert!(file_type.is_ok());
         assert_eq!(file_type.unwrap(), FileType::Ncm);
         Ok(())
